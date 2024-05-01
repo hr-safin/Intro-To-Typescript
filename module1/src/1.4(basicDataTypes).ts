4151ca53446493ba2b2066703253d40d3cc68e14
safinhr <hrsafin2434@gmail.com>
2024-05-01 20:49:58 +0600

    Basic Data types of ts

---
+++ module1/src/1.4(basicDataTypes).ts
@@ -0,0 +1,37 @@
+// Basic Data Types in typescript
+
+//string
+let fruits : string = "Apple"
+
+//number
+let number : number = 12
+
+//boolean
+let isAdmin : boolean = true
+
+//undefined
+let x : undefined = undefined
+
+//null
+let y : null = null
+
+let d : number ;
+
+d= 123
+
+
+//array
+
+let arrayOfNumber : number[]  = [12,3,4]
+
+let arrayOfName : string[] = ["safin", "hasibur", "rahman safin"]
+
+arrayOfName.push("munira")
+
+//tuple ---> order > types of values > we can control the order
+
+let ageOfNumber : [number, string, boolean] = [34, "Hasibur rahman safin", true]
+
+ageOfNumber[0] = 44
+
+console.log(ageOfNumber)
