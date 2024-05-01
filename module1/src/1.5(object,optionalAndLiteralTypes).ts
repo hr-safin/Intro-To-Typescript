6e1d613d6fe36f0b295ee599e95130fd88bb71ca
safinhr <hrsafin2434@gmail.com>
2024-05-01 21:33:48 +0600

    object, optional & Literal Types

---
+++ module1/src/1.5(object,optionalAndLiteralTypes).ts
@@ -0,0 +1,15 @@
+// Reference type > object
+
+const user : {
+    // readonly Means : we can not change the value 
+     readonly company : string // <--- Literal type (fixed every situation)
+    firstName : string;
+    middleName? : string; // <--- Optional Type
+    lastName : string;
+    age : number
+} = {
+    company : "Meta",
+    firstName : "Hasibur Rahman",
+    lastName : "Shafin",
+    age :  23
+}
