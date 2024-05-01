a5d89aa2e9c88abe025a4faadd87e27bf8a10530
safinhr <hrsafin2434@gmail.com>
2024-05-01 23:22:45 +0600

    Function in ts

---
+++ module1/src/1.6(Function).ts
@@ -0,0 +1,24 @@
+// function in ts
+
+
+function add(num1 : number, num2 : number) : number{
+    return num1 + num2
+}
+
+add(2, 2)
+
+const addArrow = (num1 : number, num2 :  number) : number =>  num1 + num2
+
+// function refer to method in object
+
+const poorUser = {
+    name : "Hasibur Rahman Safin",
+    balance : 0,
+    addBalance(balance : number) : string{
+        return `This is my new balance ${this.balance + balance}`
+    }
+}
+
+const arr :  number[] = [12,23,43]
+
+const multiArr : number[] = arr.map((elem : number) : number => elem * elem)
