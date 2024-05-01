909ab126e7f8ef57cbc6d2a44a0db99ab6fef52c
safinhr <hrsafin2434@gmail.com>
2024-05-02 00:21:34 +0600

    object destructuring in ts

---
+++ module1/src/1.8(Destructuring).ts
@@ -0,0 +1,43 @@
+//Destructuring
+
+{
+
+const user = {
+    name : {
+        firstName : "Hasibur",
+        middleName : "Rahman",
+        lastName : "Safin"
+    },
+
+    age : 23,
+    id : 21301585,
+    contact : "0132342334",
+    address : "USA in sha Allah"
+
+}
+
+const {address, name : {middleName}} =  user
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
