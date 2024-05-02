a897e4fbadf8bd16e7789988daeed485998e1003
safinhr <hrsafin2434@gmail.com>
2024-05-03 00:54:01 +0600

    ternary operator in TS

---
+++ module1/src/1.11(Ternary,Optional).ts
@@ -0,0 +1,27 @@
+// Ternary Operator, optional Chaining, nullish coalescing operator
+
+{
+    const age : number = 12
+
+    // if(age >= 18){
+    //     console.log("You are an adult")
+    // }
+
+    // else{
+    //     console.log("You are still a kid")
+    // }
+
+
+    // ternary operator
+
+    const isAdult = age >= 18 ? "Adult" : "NOT Adult"
+
+    console.log(isAdult)
+
+
+
+
+
+
+
+}
