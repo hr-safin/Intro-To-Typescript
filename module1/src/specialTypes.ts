988697353d67b0aa2e553ff8826eddbf49afe0a4
safinhr <hrsafin2434@gmail.com>
2024-05-03 15:46:50 +0600

    Special Type : Any , Unknown

---
+++ module1/src/specialTypes.ts
@@ -0,0 +1,37 @@
+// Special Type
+
+// Type Any
+
+let u = true
+
+//u = "string"
+
+//Math.round(u) // Argument of type 'boolean' is not assignable to parameter of type 'number'.
+
+let v : any = true
+
+v = "string" // No error as it can be any type
+Math.round(v) // No error as it can be any type
+
+
+// Type unknown
+
+const getSpeedInMeterPerSecond = (value : unknown) => {
+
+    if(typeof value === "number"){
+        const meterPerSecond = (value * 1000) / 3600
+        console.log(`The value is ${meterPerSecond} ms^-1`)
+    }
+    else if(typeof value === "string"){
+        const result = value.split(" ")
+        const [num, symbol] = result
+        const meterPerSecond = (parseFloat(value) * 1000) / 3600
+        console.log(`The value is ${meterPerSecond} ms^-1`)
+    }
+    else{
+        console.log("Wrong input try again")
+    }
+}
+
+getSpeedInMeterPerSecond(1000)
+getSpeedInMeterPerSecond("2000 km^-1")
