0821184b509eaee96bb86b102acad9efd61b87ea
safinhr <hrsafin2434@gmail.com>
2024-05-04 13:48:34 +0600

    Type Assertion

---
+++ module2/2.1(Type Assertion).ts
@@ -0,0 +1,46 @@
+{
+    // Type Assertion / Type Narrowing
+
+    let anything : any
+
+    anything = "Next Level Web Development";
+    anything = 22;
+
+    // (anything as number)
+
+    const kgToGm = (value : string | number) : string | number | undefined => {
+
+        if(typeof value === "string"){
+            const convertedValue = (parseFloat(value)) * 1000
+            return `The Converted Value is : ${convertedValue}`
+        }
+
+        else if(typeof value === "number"){
+            return value * 1000
+        }
+
+        else{
+            return "Wrong input"
+        }
+    }
+
+    const result1 = kgToGm(1000)  as number // type assertion
+    const result2 = kgToGm("1000")  as string  // type assertion
+
+    type CustomError = {
+        message : string
+    }
+
+    try{
+
+    }
+    catch(error){
+        console.log((error as CustomError).message)
+    }
+
+
+
+
+
+
+}
