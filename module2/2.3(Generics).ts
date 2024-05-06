cb0f7e4134f9925413240d7dd6fab6d8a60d0b90
safinhr <hrsafin2434@gmail.com>
2024-05-06 16:07:11 +0600

    Generic Type

--- module2/2.3(Generics).ts
+++ module2/2.3(Generics).ts
@@ -1,5 +1,40 @@
 {
-    // Generics
+    // Generics type
+
+    // type GenericArray = Array<number | string | boolean>
+    type GenericArray<T> = Array<T>
+
+    //const rollNumber : number[] = [12,34,5,6]
+    const rollNumber : GenericArray<number> = [12,34,5,6] // generic types
+    console.log(rollNumber)
+    // const studentName : string[] = ["Sfin", "wadif", "Akku"]
+    const studentName : GenericArray<string> = ["Sfin", "wadif", "Akku"]
+    console.log(studentName)
+    // const isAdmin : boolean[] = [true, false, true]
+    const isAdmin : GenericArray<boolean> = [true, false, true]
+    console.log(isAdmin)
+
+
+    const user : GenericArray<{name : string, age : number}> = [
+        {
+            name : "Hasibur Rahman Safin",
+            age : 23,
+        },
+        {
+            name : "Akram Hossain",
+            age : 24,
+        }
+    ]
+
+
+    // Generic Tuple
+
+    type GenericTuple<X,Y> = [X,Y]
+
+    const identity : GenericTuple<string, string> = ["Rahman", "Munira"]
+    console.log(identity)
+
+    const userWithId : GenericTuple<number, {name : string, age : number}> = [122,{name : "Hasibur", age : 23,}]
 
     
 
